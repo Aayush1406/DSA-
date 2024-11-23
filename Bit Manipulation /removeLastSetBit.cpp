34c51f3c998da35f4a748093a40364204f4c9894
Aayush1406 <90923255+Aayush1406@users.noreply.github.com>
2024-11-22 16:12:59 -0800

    Create removeLastSetBit.cpp

---
+++ Bit Manipulation /removeLastSetBit.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+#include<string>
+
+
+int main() {
+
+    int num = 16;
+    
+   num = num & (num-1);
+    
+    cout<<num;
+    
+}
