385756682cede1aa9438ac09a408273e5b27b31f
Aayush1406 <90923255+Aayush1406@users.noreply.github.com>
2024-11-22 16:24:48 -0800

    Create checkPowerof2.cpp

---
+++ Bit Manipulation /checkPowerof2.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+#include<string>
+
+
+int main() {
+
+    int num = 32;
+    
+    if ((num & num - 1) == 0){
+        cout<<"True";
+    }else{
+        cout<<"False";
+    }
+    
+}
