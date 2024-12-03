f140ad7a7b52741d19e1748908ef1b07bac748b8
Aayush1406 <90923255+Aayush1406@users.noreply.github.com>
2024-12-03 09:30:54 -0800

    Update Set Matrix Zeroes.cpp

--- Arrays/Set Matrix Zeroes.cpp
+++ Arrays/Set Matrix Zeroes.cpp
@@ -1,3 +1,6 @@
+// 1. take a DS(map) and store the indexes of rows and columns where you get 0's.
+// 2. now loop over the map and toggle the indices to 0 as per the map values.
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
