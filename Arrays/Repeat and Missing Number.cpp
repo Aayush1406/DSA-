6a0bc3536908bfabfb00a0d179436e32aff5972d
Aayush1406 <90923255+Aayush1406@users.noreply.github.com>
2024-12-16 11:27:06 -0800

    Update Repeat and Missing Number.cpp

--- Arrays/Repeat and Missing Number.cpp
+++ Arrays/Repeat and Missing Number.cpp
@@ -1,6 +1,42 @@
 #include<bits/stdc++.h>
 vector<int> findMissingRepeatingNumbers(vector < int > A) {
- 
+
+//1. Optimal
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findMissingRepeatingNumbers(vector<int> A) {
+    int n = A.size();
+    
+    long long sr = (long long)n * (n+1)/2;
+    long long sg = 0;
+
+    for(int el:A){
+        sg+=el;
+    }
+
+    long long sr2 = (long long)n * (n+1) * (2*n+1)/6;
+    long long sg2 = 0;
+
+    for(int el:A){
+        sg2 += (long long)el * (long long)el;
+    }
+
+    long long val1 = sr-sg;
+    long long val2 = sr2 - sg2;
+
+    long long val1_new = val2/val1;
+
+    long long missing = (val1_new + val1)/2;
+
+    long long repeat =  missing - val1;
+
+    vector<int> v = {(int)repeat, (int)missing};
+
+    return v;
+
+ -----------------------------------------------------------------------------------
+ //2. Not Optimal
     map<int,int> mpp;
     for(int i=0;i<A.size();i++){
         
