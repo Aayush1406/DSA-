5f7a1471e4560a6459c95632bb9f275b2884156f
Aayush1406 <90923255+Aayush1406@users.noreply.github.com>
2024-12-15 20:01:53 -0800

    Update Merge Overlapping Subinterval.cpp

--- Arrays/Merge Overlapping Subinterval.cpp
+++ Arrays/Merge Overlapping Subinterval.cpp
@@ -1,33 +1,26 @@
-// 1. sort the vector.
-// 2. compare the 1st of i with the 0th of j and if they satisfy i=j;
-
-#include<algorithm>
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
-        sort(intervals.begin(),intervals.end());  
-        
-        vector<vector<int>> vnew;
+        vector<vector<int>> v1;
+        sort(intervals.begin(), intervals.end());
 
         for(int i=0;i<intervals.size();i++){
 
             int s = intervals[i][0];
             int e = intervals[i][1];
-            
+
             for(int j=i+1;j<intervals.size();j++){
 
                 if(e>=intervals[j][0]){
-                    e = max(e, intervals[j][1]);
-                    i = j;
-                }else{
-                    break;
+                    e = max(e,intervals[j][1]);
+                    i=j;
                 }
-
             }
 
-            vnew.push_back({s,e});
+            vector<int> v2 = {s,e};
+            v1.push_back(v2);
         }
-        return vnew;
+
+        return v1;
     }
 };
